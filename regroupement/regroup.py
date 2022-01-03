634874155f740592af548a268ab403a821c1fa7d
ComeOosterhof <83632647+ComeOosterhof@users.noreply.github.com>
2022-01-03 16:26:52 +0100

    delta V matrix + notebook to script

---
+++ regroupement/regroup.py
@@ -0,0 +1,54 @@
+import numpy as np
+from numpy import linalg as LA
+
+import recoveringDebrisData2 as RDB
+
+import utils
+from utils import calcul_V
+
+import calculs_dV
+from calculs_dV import SMA_dV, INC_dV, AOP_dV
+
+debris_data = RDB.convertTLEtoDF(RDB.recoveringDebrisData())
+ordered_debris = debris_data.sort_values(by=["RAAN (rad)"], ascending=False)
+
+
+def deltaV_tot(i, j):
+    a, e, i, omega, w, M, mass = ordered_debris.values[i][0], ordered_debris.values[i][1], ordered_debris.values[i][
+        2], ordered_debris.values[i][3], ordered_debris.values[i][4], ordered_debris.values[i][5], ordered_debris.values[i][7]
+
+    a2, e2, i2, omega2, w2, M2, mass2 = ordered_debris.values[j][0], ordered_debris.values[j][1], ordered_debris.values[j][
+        2], ordered_debris.values[j][3], ordered_debris.values[j][4], ordered_debris.values[j][5], ordered_debris.values[j][7]
+
+    # Coût pour la manoeuvre sur i :
+    V1 = calcul_V(a)
+    delta_V_i = INC_dV(i, i2, V1)
+    # print(delta_V_i)
+    i = i2
+
+    # Coût pour la manoeuvre sur w :
+
+    delta_V_w = AOP_dV(w, w2, omega, a, e, i, mass)
+    # print(delta_V_w)
+    w = w2
+
+    # Coût pour la manoeuvre sur a :
+
+    delta_V_a = SMA_dV(a, a2)
+    # print(delta_V_a)
+    a = a2
+
+    delta_V_tot = delta_V_i + delta_V_w + delta_V_a
+    return delta_V_tot
+
+
+dict = {}
+deltaV_matrix = np.zeros((len(ordered_debris), len(ordered_debris)))
+for i in range(len(ordered_debris)):
+    for j in range(i+1, len(ordered_debris)):
+        dict[f"Total delta V between debris number {i} and {j} is "] = deltaV_tot(
+            i, j)
+        deltaV_matrix[i][j] = deltaV_tot(i, j)
+
+
+print(dict)
--- utilitaires/coc.py
+++ utilitaires/coc.py
@@ -1,118 +1,116 @@
-import numpy as np 
+import numpy as np
 
 from utilitaires.constants import mu_EARTH
 
-def kep2cart(coe, mu) : 
-	""" Converts coordinates of a body from orbital elements (coe) into cartesian coordinates 
 
-		inputs : 
-		------
-			- coe : array
-				Orbital elements of the body (SMA, ECC, INC, AOP, RAAN, TA)
-			- mu : float
-				Characteristic parameter of the central body 
+def kep2cart(coe, mu):
+    """ Converts coordinates of a body from orbital elements (coe) into cartesian coordinates 
 
-		outputs : 
-		-------
-			- r : array
-				Concatenation of the body's position and velocity (X, Y, Z, VX, VY, VZ) in 
-				the geocentric frame
-	"""
+            inputs : 
+            ------
+                    - coe : array
+                            Orbital elements of the body (SMA, ECC, INC, AOP, RAAN, TA)
+                    - mu : float
+                            Characteristic parameter of the central body 
 
-	a, e, i, w, RA, TA = coe
+            outputs : 
+            -------
+                    - r : array
+                            Concatenation of the body's position and velocity (X, Y, Z, VX, VY, VZ) in 
+                            the geocentric frame
+    """
 
-	h = np.sqrt(mu * a * (1 - e**2))
+    a, e, i, w, RA, TA = coe
 
-	rp = (h**2/mu)*(1/(1+e*np.cos(TA)))*np.array([np.cos(TA), np.sin(TA), 0])
-	vp = mu/h*np.array([-np.sin(TA), e+np.cos(TA), 0])
+    h = np.sqrt(mu * a * (1 - e**2))
 
+    rp = (h**2/mu)*(1/(1+e*np.cos(TA)))*np.array([np.cos(TA), np.sin(TA), 0])
+    vp = mu/h*np.array([-np.sin(TA), e+np.cos(TA), 0])
 
-	R1 = np.array([ [np.cos(w), -np.sin(w), 0.],
-			        [np.sin(w),  np.cos(w), 0.],
-			        [             0.,               0., 1.] ])
+    R1 = np.array([[np.cos(w), -np.sin(w), 0.],
+                   [np.sin(w),  np.cos(w), 0.],
+                   [0.,               0., 1.]])
 
-	R2 = np.array([ [1.,               0.,               0.],
-		            [0.,      np.cos(i),     -np.sin(i)],
-		            [0.,      np.sin(i),      np.cos(i)] ])
+    R2 = np.array([[1.,               0.,               0.],
+                   [0.,      np.cos(i),     -np.sin(i)],
+                   [0.,      np.sin(i),      np.cos(i)]])
 
-	R3 = np.array([ [np.cos(RA), -np.sin(RA),     0.],
-		            [np.sin(RA),  np.cos(RA),     0.],
-		            [             0.,               0.,     1.] ])
+    R3 = np.array([[np.cos(RA), -np.sin(RA),     0.],
+                   [np.sin(RA),  np.cos(RA),     0.],
+                   [0.,               0.,     1.]])
 
-	M = R3.dot(R2.dot(R1))
+    M = R3.dot(R2.dot(R1))
 
-	r = M.dot(rp)
-	v = M.dot(vp)
-	
-	return [r, v]
+    r = M.dot(rp)
+    v = M.dot(vp)
 
-
+    return [r, v]
 
 
 def cart2kep(R, V, mu):
-	""" Converts coordinates of a body from its cartesian coordinates into its orbitals elements (coe)
+    """ Converts coordinates of a body from its cartesian coordinates into its orbitals elements (coe)
 
-		inputs : 
-		------
-			- R : array
-				Position of the body in the ECI frame
-			- V : array
-				Velocity of the body in the ECI frame
-			- mu : float
-				Characteristic parameter of the central body 
+            inputs : 
+            ------
+                    - R : array
+                            Position of the body in the ECI frame
+                    - V : array
+                            Velocity of the body in the ECI frame
+                    - mu : float
+                            Characteristic parameter of the central body 
 
-		outputs : 
-		-------
-			- coe : array
-				Body's orbital elements (SMA, ECC, INC, AOP, RAAN, TA)
-	"""
+            outputs : 
+            -------
+                    - coe : array
+                            Body's orbital elements (SMA, ECC, INC, AOP, RAAN, TA)
+    """
 
-	r = np.linalg.norm(R)
-	v = np.linalg.norm(V)
+    r = np.linalg.norm(R)
+    v = np.linalg.norm(V)
 
-	vr = np.dot(R, V) / r
+    vr = np.dot(R, V) / r
 
-	H = np.cross(R, V)
-	h = np.linalg.norm(H)
+    H = np.cross(R, V)
+    h = np.linalg.norm(H)
 
-	i = np.arccos(H[2] / h)
+    i = np.arccos(H[2] / h)
 
-	N = np.cross([0, 0, 1], H)
-	n = np.linalg.norm(N)
+    N = np.cross([0, 0, 1], H)
+    n = np.linalg.norm(N)
 
-	if n != 0:
-		RA = np.arccos(N[0]/n)
-		if N[1] < 0:
-			RA = 2*np.pi - RA
-	else:
-		RA = 0
+    if n != 0:
+        RA = np.arccos(N[0]/n)
+        if N[1] < 0:
+            RA = 2*np.pi - RA
+    else:
+        RA = 0
 
-	E = 1/mu * ((v**2 - mu/r)*R - r*vr*V)
-	e = np.linalg.norm(E)
+    E = 1/mu * ((v**2 - mu/r)*R - r*vr*V)
+    e = np.linalg.norm(E)
 
-	if n != 0:
-		if e > 1e-10:
-			w = np.arccos(np.dot(N, E) / n / e)
-			if E[2] < 0:
-				w = 2*np.pi - w
-		else:
-			w = 0
-	else:
-		w = 0
+    if n != 0:
+        if e > 1e-10:
+            w = np.arccos(np.dot(N, E) / n / e)
+            if E[2] < 0:
+                w = 2*np.pi - w
+        else:
+            w = 0
+    else:
+        w = 0
 
-	if e > 1e-10:
-		TA = np.arccos(np.dot(E, R) / e / r)
-		if vr < 0:
-			TA = 2*np.pi - TA
-	else:
-		cp = np.cross(N, R)
-		if cp[2] >= 0:
-			TA = np.arccos(np.dot(N, R) / n / r)
-		else:
-			TA = 2*np.pi - np.arccos(np.dot(N, R) / n / r)
+    if e > 1e-10:
+        TA = np.arccos(np.dot(E, R) / e / r)
+        if vr < 0:
+            TA = 2*np.pi - TA
+    else:
+        cp = np.cross(N, R)
+        if cp[2] >= 0:
+            TA = np.arccos(np.dot(N, R) / n / r)
+        else:
+            TA = 2*np.pi - np.arccos(np.dot(N, R) / n / r)
 
-	a = h**2 / mu / (1 - e**2)
+    a = h**2 / mu / (1 - e**2)
 
-	coe = [a, e, i, w, RA, TA]
+    coe = [a, e, i, w, RA, TA]
 
-	return coe
+    return coe
